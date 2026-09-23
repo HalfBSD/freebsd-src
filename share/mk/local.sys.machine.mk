91160c5b133db895fab58c75a62390348a3af4a7
Charles Cowart <42684307+charles-cowart@users.noreply.github.com>
2026-09-22 23:33:25 -0700

    Remove RISC-V as a build target

--- share/mk/local.sys.machine.mk
+++ share/mk/local.sys.machine.mk
@@ -2,7 +2,7 @@
 .-include <site.sys.machine.mk>
 
 PSEUDO_MACHINE_LIST?= common host host32
-TARGET_MACHINE_LIST?= amd64 arm arm64 i386 powerpc riscv
+TARGET_MACHINE_LIST?= amd64 arm arm64 i386 powerpc
 
 MACHINE_ARCH_host?= ${_HOST_ARCH}
 MACHINE_ARCH_host32?= ${_HOST_ARCH32}
@@ -10,7 +10,6 @@ MACHINE_ARCH_host32?= ${_HOST_ARCH32}
 MACHINE_ARCH_LIST_arm?= armv7
 MACHINE_ARCH_LIST_arm64?= aarch64
 MACHINE_ARCH_LIST_powerpc?= powerpc64 powerpc64le ${EXTRA_ARCHES_powerpc}
-MACHINE_ARCH_LIST_riscv?= riscv64
 
 .for m in ${TARGET_MACHINE_LIST}
 MACHINE_ARCH_LIST_$m?= $m
